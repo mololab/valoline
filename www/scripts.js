1788b3be1ad73fe3c45681cd0b0592590ebb9758
ParvinEyvazov <pervin.eyvazov1999@gmail.com>
2021-08-15 16:57:57 +0300

    toast component added

--- www/scripts.js
+++ www/scripts.js
@@ -4,6 +4,35 @@ function onStartAck(epoch) {
 
 function changeStatus(status) {
   document.getElementById("loading").style.opacity = "1";
+
+  setTimeout(() => {
+    document.getElementById("loading").style.opacity = "0";
+
+    siiimpleToast
+      .setOptions({
+        container: "body",
+        class: "siiimpleToast",
+        position: "bottom|center",
+        margin: 15,
+        delay: 0,
+        duration: 3000,
+        style: {},
+      })
+      .success("Success " + status);
+
+    siiimpleToast
+      .setOptions({
+        container: "body",
+        class: "siiimpleToast",
+        position: "bottom|center",
+        margin: 15,
+        delay: 0,
+        duration: 3000,
+        style: {},
+      })
+      .alert("Error happened " + status);
+  }, 2 * 1000);
+
   // alert("status:" + status);
 }
 
